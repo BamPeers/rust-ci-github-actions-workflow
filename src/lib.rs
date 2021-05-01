45337c7f320135edaacfdc4499b94b260eb0d211
lumisce <sinehye@gmail.com>
2021-05-01 18:11:13 +0900

    Add divide function

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ pub fn multiply(a: i32, b: i32) -> i32 {
     a * b
 }
 
+/// Divides two integers
+pub fn divide(a: i32, b: i32) -> i32 {
+    a / b
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -10,5 +15,10 @@ mod test {
     #[test]
     fn test() {
         assert_eq!(multiply(2, 2), 4);
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(divide(4, 2), 2);
     }
 }
